ca0c8da2a179536223a5ce136793c79679170b9e
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-02-11 09:37:45 +0000

    exercise005 - CHECKALL + Apply Implicit Return (findNeedle comeback to)

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,4 +1,5 @@
-const findNextNumber = (nums, n) => {
+const findNextNumber = (nums, n) => (((nums.findIndex(element => element === n) === -1) || (nums.findIndex(element => element === n) === nums.length-1)) ? null : nums[nums.findIndex(element => element === n)+1] );
+const findNextNumber_InclErrProtect = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required"); // ? How do I add 'THROW NEW' to implicit return?
   if (n === undefined) throw new Error("n is required"); // ? How do I add 'THROW NEW' to implicit return?
 
@@ -20,9 +21,9 @@ const findNextNumber = (nums, n) => {
   let index = nums.findIndex(element => element === n);
   return (((index === -1) || (index === nums.length-1)) ? null : nums[index+1] ); 
 };
-const findNextNumber_implicitreturn = (nums, n) => (((nums.findIndex(element => element === n) === -1) || (nums.findIndex(element => element === n) === nums.length-1)) ? null : nums[nums.findIndex(element => element === n)+1] );
 
-const count1sand0s = str => {
+const count1sand0s = str => str.split("").reduce((binarynums, num) => { binarynums[num] = binarynums[num] ? binarynums[num] + 1 : 1; return binarynums;}, {"0" : 0});
+const count1sand0s_InclErrProtect = str => {
   if (str === undefined) throw new Error("str is required"); // ? How do I add 'THROW NEW' to implicit return?
 
   // v1 - Grouping Object
@@ -35,21 +36,21 @@ const count1sand0s = str => {
   // v2 - Reduce (with hc default)
   return str.split("").reduce((binarynums, num) => { binarynums[num] = binarynums[num] ? binarynums[num] + 1 : 1; return binarynums;}, {"0" : 0});   
 };
-const count1sand0s_implicitreturn = str => str.split("").reduce((binarynums, num) => { binarynums[num] = binarynums[num] ? binarynums[num] + 1 : 1; return binarynums;}, {"0" : 0});
 
-const reverseNumber = n => {
+const reverseNumber = n => parseInt(n.toString().split("").reverse().join(""));
+const reverseNumber_InclErrProtect = n => {
   if (n === undefined) throw new Error("n is required"); // ? How do I add 'THROW NEW' to implicit return?
   return parseInt(n.toString().split("").reverse().join(""));
 };
-const reverseNumber_implicitreturn = n => parseInt(n.toString().split("").reverse().join(""));
 
-const sumArrays = arrs => {
+const sumArrays = arrs => arrs.reduce((lhs, rhs_item) => lhs + rhs_item.reduce((lhs, rhs_item) => lhs + rhs_item, 0), 0);
+const sumArrays__InclErrProtect = arrs => {
   if (arrs === undefined) throw new Error("arrs is required"); // ? How do I add 'THROW NEW' to implicit return?
   return arrs.reduce((lhs, rhs_item) => lhs + rhs_item.reduce((lhs, rhs_item) => lhs + rhs_item, 0), 0);
 };
-const sumArrays_implicitreturn = arrs => arrs.reduce((lhs, rhs_item) => lhs + rhs_item.reduce((lhs, rhs_item) => lhs + rhs_item, 0), 0);
 
-const arrShift = arr => {
+const arrShift = arr => (arr.length > 1) ? [arr[arr.length-1]].concat(arr.slice(1, arr.length-1)).concat([arr[0]]): arr;
+const arrShift_InclErrProtect = arr => {
   if (arr === undefined) throw new Error("arr is required"); // ? How do I add 'THROW NEW' to implicit return?
 
   // v1
@@ -60,32 +61,35 @@ const arrShift = arr => {
   // return arr;
 
   // v2 - Create array with end items swapped, everything same in middle
-  if (arr.length > 0) { [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]]; }
+  if (arr.length > 1) { [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]]; }
   return arr;
 
-  // v3 - LastItem + ConcatSlice + Concat FirstItem NOT WORKING FOR ARR[] ?? COME BACK TO ??
-  // return (arr.length > 0) ? [arr[arr.length-1]].concat(arr.slice(1, arr.length-1)).concat([arr[0]]): arr;
+  // v3 - LastItem + ConcatSlice + Concat FirstItem
+  // return (arr.length > 1) ? [arr[arr.length-1]].concat(arr.slice(1, arr.length-1)).concat([arr[0]]): arr;
 };
-const arrShift_implicitreturn = arr => (arr.length > 0) ? [arr[arr.length-1]].concat(arr.slice(1, arr.length-1)).concat([arr[0]]): arr;
-
 
 const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required"); 
  
+  // // v1
   for (let key in haystack) {
     if (haystack[key].toString().toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0 ) {
       return true;
     }
   }
   return false;
+
+  // v2 - COMEBACK TO
+  // return Object.keys(haystack).reduce((previous,key) =>{ (haystack[key].toString().toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0 ) ? true : 0; return previous;},false);
 };
 
-const getWordFrequencies = str => {
+const getWordFrequencies = str => str.replace(/[^a-zA-Z0-9 ]/g, '').toLowerCase().split(" ").reduce((wordfeq, word) => { wordfeq[word] = wordfeq[word] ? wordfeq[word] + 1 : 1; return wordfeq;}, {});
+const getWordFrequencies_InclErrProtect = str => {
   if (str === undefined) throw new Error("str is required"); // ? How do I add 'THROW NEW' to implicit return?
   return str.replace(/[^a-zA-Z0-9 ]/g, '').toLowerCase().split(" ").reduce((wordfeq, word) => { wordfeq[word] = wordfeq[word] ? wordfeq[word] + 1 : 1; return wordfeq;}, {});
 };
-const getWordFrequencies_implicitreturn = str => str.replace(/[^a-zA-Z0-9 ]/g, '').toLowerCase().split(" ").reduce((wordfeq, word) => { wordfeq[word] = wordfeq[word] ? wordfeq[word] + 1 : 1; return wordfeq;}, {});
+
 
 module.exports = {
   findNextNumber,
--- test/exercise005.test.js
+++ test/exercise005.test.js
@@ -140,7 +140,7 @@ describe("findNeedle", () => {
 });
 
 describe("getWordFrequencies", () => {
-  test.only("returns the frequencies of each word in a string", () => {
+  test("returns the frequencies of each word in a string", () => {
     expect(getWordFrequencies("hello world")).toEqual({
       hello: 1,
       world: 1
@@ -160,13 +160,13 @@ describe("getWordFrequencies", () => {
     });
   });
 
-  test.only("ignores capitalisation", () => {
+  test("ignores capitalisation", () => {
     expect(getWordFrequencies("Hello hello hello")).toEqual({
       hello: 3
     });
   });
 
-  test.only("ignores punctuation", () => {
+  test("ignores punctuation", () => {
     // Hint: Google "JavaScript remove special characters from string" to get some ideas!
     expect(
       getWordFrequencies("Hello, hello hello! What have we here?")
