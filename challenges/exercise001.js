9063362d78f2fed3efd6d6f7add9fe4f327a6165
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-02-14 16:35:35 +0000

    exercise001 - addVAT+countLinuxUsers replaced, comment reductions

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,8 +1,7 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
   
-// RegEX - replace(/RegEX/g, letter => letter.<operation>())
-// RegEX => [^\w{1}:FirstCharOfFirstWord]
+  // v1 - RegEX => [^\w{1}:FirstCharOfFirstWord]
   return word.replace(/^\w{1}/g, letter => letter.toUpperCase());
 }
 
@@ -15,42 +14,24 @@ function generateInitials(firstName, lastName) {
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  
-  // 10 ^ DecimalPlaces
-  const dp = Math.pow(10,2);
-  return Math.round((originalPrice * (1+(vatRate/100))) * dp) / dp;
+
+  // v1 - string conversion (slow/neat, no Math) 
+  return Number((originalPrice * (1+(vatRate/100))).toFixed(2));
+
+  // v2 - Power calculation (fast, use Math)
+  // return Math.round((originalPrice * (1+(vatRate/100))) * Math.pow(10,2)) / Math.pow(10,2);
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  
-  // Option1 : ReCalc
-  //const dp = Math.pow(10,2);
-  //return Math.round((originalPrice * (1-(reduction/100))) * dp) / dp; 
-
-  // Option 2 : Reuse addVAT ( & its validation) 
   return addVAT(originalPrice,-reduction);
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
 
-  // FIRST ATTEMPT
-  // let start = 0;
-  // let stop = 0;
-  // if ((str.length % 2) === 0) {
-  //   // even
-  //   start = Math.floor((str.length)/2) - 1
-  //   stop = start + 2
-  // }
-  // else {
-  //   // odd
-  //   start = Math.floor((str.length)/2)
-  //   stop = start + 1    
-  // }
-
-  // SECOND CLEANUP
+  // v1 - Odd length, starts -1, stops +2 : Even length, starts 0, stops +1
   let start = Math.floor((str.length)/2) + (((str.length % 2) === 0)?-1:0);
   let stop = start + (((str.length % 2) === 0)?2:1);
   return str.substring(start,stop);
@@ -63,48 +44,23 @@ function reverseWord(word) {
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  
-  // Scruffy!
-  // for(let i=0;i<words.length;i++) {
-  //     words[i] = reverseWord(words[i]);
-  // }
-
-  // Neater? However, why MUST return on seperate line below?
-  words.forEach((word,index) => words[index] = reverseWord(word));
-
-  return words;
+  return words.map((word,index) => words[index] = reverseWord(word));
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  
-  // SetUp Empty
-  let toFind = "Linux"
-  let group = [];
-  group[toFind] = 0;
-
-  // Count instances of all
-  for(let i=0;i<users.length;i++) {
-    group[users[i].type] = group[users[i].type] ? group[users[i].type] + 1 : 1; 
-  }
-  
-  // predefine Linux return
-  return group[toFind];
+  return users.filter(user => user.type === 'Linux').length;
 }
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
-
   const dp = Math.pow(10,2);
-
-  // Reduce Sums A + B for every item in array
   const sum = scores.reduce((a,b) => a + b, 0);
   return Math.round((sum / scores.length) * dp) / dp;
 }
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
-
   let reply = "";
   if ((n % 3) === 0) {
     reply += "fizz";  
